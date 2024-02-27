217ba9303a1c95b6bab6ec0a44c9975e20aa945f
magnetika-elab <161363335+magnetika-elab@users.noreply.github.com>
2024-02-27 10:19:56 -0500

    Add files via upload

--- piPicoSave.py
+++ piPicoSave.py
@@ -1,7 +1,5 @@
 import os, re, time, pyautogui
-import tkinter as tk
-from tkinter import messagebox
-
+import platform
 
 def waitToRun(delay):
     print("Executing Picolog Backup in {0} seconds...".format(delay))
@@ -14,41 +12,83 @@ def moveClick(x,y, delay = .25):
     pyautogui.click(x, y)
     time.sleep(.25)
 
-
-def get_sequence():
-    with open('mouse_recording.txt', 'r') as file:
-        lines = file.read().strip().splitlines()
-    sequence = [
-        tuple(map(int, line.split(','))) if re.match(r'\d+,\d+', line) else line
-        for line in lines
-    ]
-    return sequence
+def determine_environment():
+    uname_results = platform.uname()
+    string = f'{uname_results.node} is {uname_results.system} {uname_results.release} on {uname_results.machine}'
+    print(string)
 
 
-def show_message(title, message):
-    messagebox.showinfo(title, message)
 
+def load_button_images(supplied_button_directory=None):
+    uname_results = platform.uname()
+    button_image_directory = (
+        os.path.join(os.getcwd(), 'on_screen_buttons', uname_results.system, uname_results.machine)
+        if supplied_button_directory is None
+        else supplied_button_directory
+    )
+    button_images = [
+        (filename[:filename.index('.')], os.path.join(button_image_directory, filename)) 
+        for filename in os.listdir(button_image_directory) 
+        if filename.endswith('.png')]
+    button_dict = {}
+    for button_name, button_path in button_images:
+        button_dict[button_name] = button_path
+    return button_dict
+
+class ButtonNotFoundException(Exception):
+    def __init__(self, button_name):
+        self.button_name = button_name
+        super().__init__(f"Button '{button_name}' not found on screen.")
+
+def find_on_screen(button_image_path):
+    try:
+        button_location = pyautogui.locateCenterOnScreen(button_image_path)
+    except Exception as e:
+        button_location = None
+    return button_location
+
+
+def handle_sequence_item(sequence_item, button_dictionary):
+    main_action, fallback_action = sequence_item
+    action_type, action_required, action_input = main_action
+    action_optional = False if action_required == 'required' else True
+    if action_type == 'button':
+        button_image_path = button_dictionary[action_input]
+        button_location = find_on_screen(button_image_path)
+        if button_location is not None:
+            pyautogui.click(button_location)
+        elif button_location is None and not action_optional and fallback_action is not None:
+            handle_sequence_item((fallback_action, None), button_dictionary)
+        elif button_location is None and not action_optional and fallback_action is None:
+            raise ButtonNotFoundException(action_input)
+    elif action_type == 'text':
+        pyautogui.typewrite(str(action_input))
+
+
+def run_sequence(sequence, button_dictionary, delay_after_action = 1):
+    for sequence_item in sequence:
+        handle_sequence_item(sequence_item, button_dictionary)
+        time.sleep(delay_after_action)
 
 def main():
     waitToRun(10)
-    try:
-        sequence = get_sequence()
-    except FileNotFoundError:
-        root = tk.Tk()
-        root.withdraw()  # Hides the main window
-        # Show the message box
-        show_message("Error", "Must run setup_mouse_clicks.py before auto backup can function")
-        root.mainloop()
+    determine_environment()
+    button_dictionary = load_button_images()
+    if find_on_screen(button_dictionary['save_icon_lightmode']) is not None:
+        color_mode = 'lightmode'
+    elif find_on_screen(button_dictionary['save_icon_darkmode']) is not None:
+        color_mode = 'darkmode'
+    else:
+        exit()
+    sequence = [
+        (('button', 'required', f'save_icon_{color_mode}'),      None),
+        (('button', 'required', f'save_data_text_{color_mode}'), None),
+        (('button', 'optional', f'continue_{color_mode}'),       None),
+        (('text',    None,      'BACKUP'),                       None),
+        (('button', 'required', f'final_save_{color_mode}'),     ('button', 'required', f'save_and_overwrite_{color_mode}')),
+    ]
+    run_sequence(sequence, button_dictionary)
 
-    for entry in sequence:
-        if type(entry) == tuple:
-            x, y = entry
-            moveClick(x, y, delay=.5)
-            time.sleep(1)
-        elif type(entry) == str:
-            if entry == 'filename':
-                pyautogui.typewrite('BACKUP')
-    return
 
 if __name__ == '__main__':
     main()
